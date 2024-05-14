79f38e65f98dcdd3896e5a6f3f54ada20f57184c
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-05-14 21:03:08 +0700

    chore: Update App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+
+  const handleClick = (x) => {
+    fetch(`http://localhost:8000/control/${x}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    console.log(x + ": OK")
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <div className='ful'>
+        <h1 className='label'>Điều khiển đèn led - Bài 4</h1>
+      </div>
+      <div className='ful'>
+        <button className='btn' onClick={()=>handleClick(1)}>Bật led</button>
+        <button className='btn' onClick={()=>handleClick(0)}>Tắt led</button>
+      </div>
     </div>
   );
 }
